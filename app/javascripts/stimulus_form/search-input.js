ac1a6f68f37c30b58e39c35944dfcd7e053f798d
qinmingyuan <tomq@everymarket.com>
2025-12-24 13:15:32 +0800

    submit

--- app/javascripts/stimulus_form/search-input.js
+++ app/javascripts/stimulus_form/search-input.js
@@ -3,27 +3,30 @@ import BaseController from '../base_controller'
 export default class extends BaseController {
   static targets = ['input', 'content']
   static values = {
-    url: String
+    url: String,
+    auto: { type: Boolean, default: false}
   }
 
   connect() {
-    this.initInput()
+    if (this.autoValue) {
+      this.initInput()
+    }
   }
 
   initInput() {
     const ele = this.inputTarget
-    ele.addEventListener('input', this.form)
+    ele.addEventListener('input', this.submit)
     ele.addEventListener('compositionstart', event => {
-      event.target.removeEventListener('input', this.form)
+      event.target.removeEventListener('input', this.submit)
     })
     ele.addEventListener('compositionend', event => {
-      event.target.addEventListener('input', this.form)
+      event.target.addEventListener('input', this.submit)
       this.conForm(ele)
     })
   }
 
   // NOTICE  here this becomes ele, who call addEventListener
-  form() {
+  submit() {
     const con = this.closest('[data-controller~=search-input]').getController('search-input')
 
     if (con.hasUrlValue) {
