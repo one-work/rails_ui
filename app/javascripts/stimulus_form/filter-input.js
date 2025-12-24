45a00b95c61dbe68fe20dd8071834253edb4b537
qinmingyuan <tomq@everymarket.com>
2025-12-24 14:22:30 +0800

    usd disblaed

--- app/javascripts/stimulus_form/filter-input.js
+++ app/javascripts/stimulus_form/filter-input.js
@@ -15,12 +15,21 @@ export default class extends BaseController {
   }
 
   submitButton(e) {
+    e.preventDefault()
     const el = e.currentTarget
+    const items = el.dataset.item.split(',')
+    console.debug('ITEMS' ,items)
+
     for (const input of el.form.elements) {
-      if (input.name === el.dataset.item && input.type === 'hidden') {
-        input.remove()
+
+      if (items.includes(input.name) && input.type === 'hidden') {
+        console.debug('to delete----', input.name)
+        input.disabled = true
+      } else {
+        console.debug('dddd------------', input.name, input.type)
       }
     }
+    el.form.requestSubmit()
   }
 
   confirm(e) {
