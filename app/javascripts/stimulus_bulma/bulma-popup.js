fc49019365f01b683c9fd6d9b425a0c56984dac4
qinmingyuan <tomq@everymarket.com>
2025-12-24 20:31:43 +0800

    support nornal popu

--- app/javascripts/stimulus_bulma/bulma-popup.js
+++ app/javascripts/stimulus_bulma/bulma-popup.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['popup']
+  static targets = ['popup', 'overlay']
 
   connect() {
     //document.documentElement.classList.add('clipped')
@@ -10,5 +10,15 @@ export default class extends Controller {
   close(e) {
     e.currentTarget.remove()
     this.popupTarget.remove()
+  }
+
+  open() {
+    this.overlayTarget.classList.remove('display-none')
+    this.popupTarget.classList.remove('display-none')
+  }
+
+  hidden(e) {
+    e.currentTarget.classList.add('display-none')
+    this.popupTarget.classList.add('display-none')
   }
 }
