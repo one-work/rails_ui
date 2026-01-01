fc1142c8f743541c3d2579cffbe8591cc0a42978
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-01 12:38:54 +0800

    antv line

--- app/javascripts/stimulus_antv/antv-line.js
+++ app/javascripts/stimulus_antv/antv-line.js
@@ -9,22 +9,27 @@ export default class extends AntvBaseController {
         x: 'title',
         y: 'value'
       },
+      axis: {
+        y: {
+          title: false,
+          labelFormatter: (v) => `${v}`.replace(/\d{1,3}(?=(\d{3})+$)/g, (s) => `${s},`)
+        }
+      },
       children: [
         {
           type: 'line',
+          encode: { shape: 'smooth' },
           labels: [{ text: "value", style: { dx: -10, dy: -12 } }]
         },
         {
           type: 'point',
           style: { fill: "white" },
+          encode: { shape: 'point' },
           tooltip: false
         }
       ]
     })
 
-    //this.chart.axis('y', {title: false, labelFormatter: (v) => `${v}`.replace(/\d{1,3}(?=(\d{3})+$)/g, (s) => `${s},`)})
-    //this.chart.line().encode('shape', 'smooth')
-    //this.chart.point().encode('shape', 'point').tooltip(false)
     this.chart.render()
   }
 
