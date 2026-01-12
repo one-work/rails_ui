981562d1ceff8a28ce42f4ba9556d26be5a93484
qinmingyuan <mingyuan0715@foxmail.com>
2026-01-12 10:37:34 +0800

    support both iocn and button

--- app/javascripts/stimulus_com/tr-actions.js
+++ app/javascripts/stimulus_com/tr-actions.js
@@ -2,33 +2,54 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static values = {
-    template: { type: String, default: 'template_tr_action' },
+    action: { type: String, default: 'template_tr_action' },
+    link: { type: String, default: 'template_tr_link' },
     data: Array
   }
 
   connect() {
-    const template = document.getElementById(this.templateValue)
+    const templateAction = document.getElementById(this.actionValue)
+    const templateLink = document.getElementById(this.linkValue)
     const td = document.createElement('td')
     const div = document.createElement('div')
     div.dataset.showTarget = 'item'
     div.classList.add('visibility-hidden')
 
     this.dataValue.forEach(item => {
-      const fragment = template.content.cloneNode(true)
-      const form = fragment.querySelector('form')
-      form.action = item.action
+      if (item.action) {
+        const fragment = templateAction.content.cloneNode(true)
+        const form = fragment.querySelector('form')
+        form.action = item.action
 
-      const button = fragment.querySelector('button')
-      button.ariaLabel = item.title
+        const button = fragment.querySelector('button')
+        button.ariaLabel = item.title
 
-      const use = fragment.querySelector('use')
-      use.setAttribute('href', `${use.href.baseVal}#${item.icon}`)
+        this.#setIcon(fragment, item.icon)
 
-      div.appendChild(fragment)
+        div.appendChild(fragment)
+      } else {
+        const fragment = templateLink.content.cloneNode(true)
+        const a = fragment.querySelector('a')
+        a.href = item.href
+
+        const span = fragment.querySelector('span')
+        span.innerText = item.title
+
+        this.#setIcon(fragment, item.icon)
+      }
     })
 
     td.appendChild(div)
     this.element.appendChild(td)
+  }
+
+  #setIcon(fragment, icon) {
+    const use = fragment.querySelector('use')
+    if (icon) {
+      use.setAttribute('href', `${use.href.baseVal}#${icon}`)
+    } else {
+      use.parentNode.remove()
+    }
   }
 
 }
