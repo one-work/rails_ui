f8ebe9e083c623bd6bef2d490501406259009c30
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-08 12:21:35 +0800

    test if someoen is back

--- app/javascripts/stimulus_com/appear.js
+++ app/javascripts/stimulus_com/appear.js
@@ -3,11 +3,13 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
 
   connect() {
-    alert('connected')
-  }
-
-  disconnect() {
-    alert('disconnected')
+    document.addEventListener('visibilitychange', () => {
+      if (document.visibilityState === 'hidden') {
+        document.addEventListener('visibilitychange', ()  => {
+          alert('你回来了')
+        }, { once: true })
+      }
+    }, { once: true })
   }
 
 }
