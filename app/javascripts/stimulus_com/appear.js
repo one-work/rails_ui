e785442bcda973de92b51334bf7ce027b13ac661
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-08 12:30:20 +0800

    fix apper vevent

--- app/javascripts/stimulus_com/appear.js
+++ app/javascripts/stimulus_com/appear.js
@@ -5,8 +5,10 @@ export default class extends Controller {
   connect() {
     document.addEventListener('visibilitychange', () => {
       if (document.visibilityState === 'hidden') {
-        document.addEventListener('visibilitychange', ()  => {
-          alert('你回来了')
+        document.addEventListener('visibilitychange', () => {
+          if (document.visibilityState === 'visible') {
+            alert('你回来了')
+          }
         }, { once: true })
       }
     }, { once: true })
