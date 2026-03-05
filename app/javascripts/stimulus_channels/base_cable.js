1e785cb3ca08b8bb62003a60a7bf3d7a6afe3633
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-05 15:14:37 +0800

    cahgne cable

--- app/javascripts/stimulus_channels/base_cable.js
+++ app/javascripts/stimulus_channels/base_cable.js
@@ -2,7 +2,8 @@ import { Controller } from '@hotwired/stimulus'
 import { createConsumer } from '@rails/actioncable/src'
 
 export default class BaseCable extends Controller {
-  static consumer = createConsumer()
+
+  static consumer = createConsumer('/rails/cable')
 
 }
 
