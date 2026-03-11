b2e9d7fce8d11784cbd44159a64036f673970ed5
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-11 23:02:05 +0800

    turbo logic

--- app/javascripts/rails_design/index.js
+++ app/javascripts/rails_design/index.js
@@ -1,13 +1,12 @@
-import '@hotwired/turbo-rails'
 import { Idiomorph } from 'idiomorph/dist/idiomorph.esm'
 window.Idiomorph = Idiomorph
 
 import './dataset'
 import './utils'
 
+import '../turbo'
 import '../stimulus_base'
 import '../stimulus_com'
 import '../stimulus_channels'
 import '../stimulus_bulma'
 import '../stimulus_form'
-import '../turbo'
--- app/javascripts/turbo/index.js
+++ app/javascripts/turbo/index.js
@@ -1,2 +1,9 @@
+import '@hotwired/turbo-rails'
 import './events'
 import './override'
+
+Turbo.StreamActions.visit = () => {
+  const url = this.getAttribute('url')
+
+  Turbo.visit(url)
+}
