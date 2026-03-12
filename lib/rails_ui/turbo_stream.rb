6c355c0b1593f9ed4a7bf4506de58669091e4f39
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-12 22:00:56 +0800

    return ulr

--- lib/rails_ui/turbo_stream.rb
+++ lib/rails_ui/turbo_stream.rb
@@ -7,7 +7,7 @@ module RailsUi
       turbo_stream_action_tag :append_body, template: template
     end
 
-    def visit(url: '/')
+    def visit(url: request.url)
       turbo_stream_action_tag :visit, url: url
     end
 
