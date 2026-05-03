0bdcc1764db8748b4d4fbc9ab1a0a154ac9740c9
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-03 22:54:22 +0800

    fix

--- app/javascripts/stimulus_wx/wxpay.js
+++ app/javascripts/stimulus_wx/wxpay.js
@@ -37,7 +37,7 @@ export default class extends Controller {
         this.paramsValue,
         res => {
           if (res.err_msg === 'get_brand_wcpay_request:ok') {
-            Turbo.visit(location.href)
+            Turbo.visit(location, { action: 'replace' })
           }
         })
     })
