e0c59d1f32bd75137135be9287ab8c22519e06ad
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-24 14:49:34 +0800

    fix

--- app/javascripts/stimulus_douyin/douyin-program.js
+++ app/javascripts/stimulus_douyin/douyin-program.js
@@ -3,14 +3,20 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static values = {
     url: String, // url must begin with /pages
-    data: Object,
-    direct: Boolean,
-    launch: Boolean,
-    nav: Boolean,
-    debug: Boolean
+    direct: Boolean
   }
 
   connect() {
+    if (this.directValue) {
+      this.noticeUrl()
+    }
+  }
 
+  noticeUrl() {
+    tt.miniProgram.postMessage({
+      data: {
+        url: this.urlValue
+      }
+    })
   }
 }
