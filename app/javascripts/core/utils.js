9ea8b537adfd222732dc0b1bb8cc79a908a947da
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-26 00:58:51 +0800

    fix utils

--- app/javascripts/core/utils.js
+++ app/javascripts/core/utils.js
@@ -1,9 +1,14 @@
 window.utils = {
   xx: (date) => {
-    const format = new Intl.DateTimeFormat('zh-Hans-CN', {
-      calendar: 'chinese',
-      timeZone: 'Asia/Shanghai'
-    })
+    const format = new Intl.DateTimeFormat(
+      'zh-Hans-CN',
+      {
+        calendar: 'chinese',
+        timeZone: 'Asia/Shanghai',
+        dateStyle: 'full',
+        timeStyle: 'full'
+      }
+    )
     return format.format(date)
   }
 }
