9f34410e82692b8a1f55a5b086e486fbbb5f2156
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-26 01:16:04 +0800

    fix

--- app/javascripts/wxwork_script.js
+++ app/javascripts/wxwork_script.js
@@ -1,37 +1,31 @@
-import '../weixin_script'
 function wxwork_fetch({ url = location.href } = {}) {
-  weixin_fetch({
-    url: url,
-    success: () => {
-      fetch('/wechat/agent_js', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ url: url })
-      }).then(response => {
-        return response.json()
-      }).then(body => {
-        wx.agentConfig({
-          corpid: body['corpid'],
-          agentid: body['agentid'],
-          timestamp: body['timestamp'],
-          nonceStr: body['noncestr'],
-          signature: body['signature'],
-          jsApiList: body['apis'],
-          success: res => {
-            if (body['debug']) {
-              alert('wx.agentConfig success' + JSON.stringify(res))
-            } else {
-              console.debug('wx.agentConfig success', JSON.stringify(res))
-            }
-          },
-          fail: res => {
-            new Error('wx.agentConfig fail ' + JSON.stringify(res))
-          }
-        })
-      })
-    }
+  fetch('/wechat/agent_js', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ url: url })
+  }).then(response => {
+    return response.json()
+  }).then(body => {
+    wx.agentConfig({
+      corpid: body['corpid'],
+      agentid: body['agentid'],
+      timestamp: body['timestamp'],
+      nonceStr: body['noncestr'],
+      signature: body['signature'],
+      jsApiList: body['apis'],
+      success: res => {
+        if (body['debug']) {
+          alert('wx.agentConfig success' + JSON.stringify(res))
+        } else {
+          console.debug('wx.agentConfig success', JSON.stringify(res))
+        }
+      },
+      fail: res => {
+        new Error('wx.agentConfig fail ' + JSON.stringify(res))
+      }
+    })
   })
 }
