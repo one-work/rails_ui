86e568a77ba922a1fbc9d364358057ad36e06d0d
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-29 19:39:21 +0800

    fix

--- lib/rails_ui/engine.rb
+++ lib/rails_ui/engine.rb
@@ -1,11 +1,5 @@
 module RailsUi
   class Engine < ::Rails::Engine
 
-    initializer 'rails_ui.assets' do |app|
-      app.config.assets.paths += [
-        root.join('app/assets/stylesheets')
-      ]
-    end
-
   end
 end
