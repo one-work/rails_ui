94a861b614d8f60264d875b92175584f268d8ed3
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-29 19:58:57 +0800

    consumer

--- app/javascripts/stimulus/channels/session_init_channel.js
+++ app/javascripts/stimulus/channels/session_init_channel.js
@@ -3,15 +3,23 @@ import { createConsumer } from '@rails/actioncable/src'
 
 // 专门用于微信扫码登录
 export default class extends BaseCable {
+  static values = {
+    url: String
+  }
 
   connect() {
     this.subscribe()
   }
 
   subscribe() {
-    const consumer = createConsumer('https://linlishenghuo.com/cable')
-    const urlParams = new URLSearchParams(location.search)
+    let consumer
+    if (this.hasUrlValue) {
+      consumer = createConsumer(this.urlValue)
+    } else {
+      consumer = BaseCable.consumer
+    }
 
+    const urlParams = new URLSearchParams(location.search)
     this.subscription = consumer.subscriptions.create(
       {
         channel: 'Wechat::SessionInitChannel',
