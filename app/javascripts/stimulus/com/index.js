472ec09fbd89f8bb8efd0fe0f1b0baead075091e
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-13 16:28:25 +0800

    app js

--- app/javascripts/stimulus/com/index.js
+++ app/javascripts/stimulus/com/index.js
@@ -10,6 +10,9 @@ application.register('animate-scroll', AnimateScrollController)
 import AnimateTyperController from './animate-typer'
 application.register('animate-typer', AnimateTyperController)
 
+import AppController from './app'
+application.register('app', AppController)
+
 import AppearController from './appear'
 application.register('appear', AppearController)
 
