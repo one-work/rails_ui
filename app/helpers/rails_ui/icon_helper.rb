6e5243b814615fe68715af3fcee97d3aa946e57f
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-21 12:40:04 +0800

    auto commit

--- app/helpers/rails_ui/icon_helper.rb
+++ app/helpers/rails_ui/icon_helper.rb
@@ -14,7 +14,7 @@ module RailsUi
     end
 
     def svg_data_url(path = 'placeholder.svg')
-      svg_content = Rails.application.assets.resolver.read path
+      svg_content = Rails.app.assets.resolver.read path
       base64_svg = Base64.strict_encode64(svg_content)
       "data:image/svg+xml;base64,#{base64_svg}"
     end
