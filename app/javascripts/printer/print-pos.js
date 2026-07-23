189e12ccdad907e9a54a2807ca994eef36f85408
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-23 12:31:44 +0800

    fix

--- app/javascripts/printer/print-pos.js
+++ app/javascripts/printer/print-pos.js
@@ -3,10 +3,13 @@ import { PrintPOS } from 'xcprinter'
 
 export default class extends Controller {
 
-
-
   print() {
+    const hash = {}
+    this.element.querySelectorAll('[data-print]').forEach(item => {
+      hash[item.dataset.print] = item.innerText
+    })
 
+    window.xx = hash
   }
 
 }
