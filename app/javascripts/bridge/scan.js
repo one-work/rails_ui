37d04ff456b7a23bd33f34f93d0293d547b4babd
qinmingyuan <mingyuan0715@foxmail.com>
2026-08-13 23:32:45 +0800

    fix

--- app/javascripts/bridge/scan.js
+++ app/javascripts/bridge/scan.js
@@ -2,6 +2,10 @@ import { BridgeComponent } from '@hotwired/hotwire-native-bridge'
 
 export default class extends BridgeComponent {
   static component = 'scan'
+  static values = {
+    form: String,
+    params: Object
+  }
 
   connect() {
     super.connect()
@@ -20,6 +24,26 @@ export default class extends BridgeComponent {
       console.debug('', message)
       const value = message.data.value
       if (value) {
+        let url = this.element.dataset.reportUrl
+        let body
+        if (this.hasFormValue) {
+          const form = document.getElementById(this.formValue)
+          body = new FormData(form)
+          url = form.action
+        } else {
+          body = new FormData()
+        }
+        if (this.hasParamsValue) {
+          Object.keys(this.paramsValue).forEach(k => {
+            body.append(k, this.paramsValue[k])
+          })
+        }
+        const csrfToken = document.querySelector('meta[name=csrf-token]').content
+        fetch(url, {
+          method: 'POST',
+          body: body,
+          headers: { 'X-CSRF-Token': csrfToken }
+        }).then(response => response.text()).then(body => Turbo.renderStreamMessage(body))
       }
     })
   }
