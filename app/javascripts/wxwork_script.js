13ff496a47197220e026b222aa2016de037b0241
qinmingyuan <mingyuan0715@foxmail.com>
2026-08-17 16:08:25 +0800

    fixed work js logic

--- app/javascripts/wxwork_script.js
+++ app/javascripts/wxwork_script.js
@@ -1,5 +1,5 @@
-function wxwork_fetch({ url = location.href } = {}) {
-  fetch('/wechat/agent_js', {
+function wxwork_fetch(configUrl, { url = location.href } = {}) {
+  fetch(configUrl, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
@@ -27,5 +27,25 @@ function wxwork_fetch({ url = location.href } = {}) {
         new Error('wx.agentConfig fail ' + JSON.stringify(res))
       }
     })
+
+    wx.ready()
   })
 }
+
+(() => {
+  const weixin_script = document.createElement('script')
+  weixin_script.src = 'https://res.wx.qq.com/open/js/jweixin-1.2.0.js'
+  weixin_script.async = true
+  document.head.appendChild(weixin_script)
+  weixin_script.addEventListener('load', () => {
+    weixin_fetch('/wechat/js')
+  })
+
+  const work_script = document.createElement('script')
+  work_script.src = 'https://open.work.weixin.qq.com/wwopen/js/jwxwork-1.0.0.js'
+  work_script.async = true
+  document.head.appendChild(work_script)
+  work_script.addEventListener('load', () => {
+    wxwork_fetch('/wechat/agent_js')
+  })
+})()
