e9e38674d7b10007033be4af4434a8cad2cbf24e
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-04 23:23:45 +0800

    support config

--- app/javascripts/stimulus/com/appear.js
+++ app/javascripts/stimulus/com/appear.js
@@ -3,11 +3,13 @@ import BaseController from '../base_controller'
 export default class extends BaseController {
 
   connect() {
-    document.addEventListener('visibilitychange', () => {
-      if (document.visibilityState === 'hidden') {
-        this.post('/auth/board/user/autonym_wait')
-      }
-    }, { once: true })
+    if (this.hasUrlValue) {
+      document.addEventListener('visibilitychange', () => {
+        if (document.visibilityState === 'hidden') {
+          this.post(this.urlValue)
+        }
+      }, { once: true })
+    }
   }
 
 }
