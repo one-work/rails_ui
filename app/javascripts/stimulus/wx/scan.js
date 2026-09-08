ccf3aeed2c3493352c4e6757e96cfe7681d1d01b
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-08 09:10:47 +0800

    fix scan logic

--- app/javascripts/stimulus/wx/scan.js
+++ app/javascripts/stimulus/wx/scan.js
@@ -49,6 +49,7 @@ export default class extends BaseController {
   }
 
   form(e) {
+    const ele = e.currentTarget
     const form = document.getElementById(this.formValue)
     const body = new FormData(form)
     const url = ele.dataset.reportUrl || form.action
