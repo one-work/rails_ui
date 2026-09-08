b4b6031eaac390dc8bd1e5d1ecc1b4c8f0ab2831
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-08 09:29:43 +0800

    fixed

--- app/javascripts/stimulus/com/history.js
+++ app/javascripts/stimulus/com/history.js
@@ -3,6 +3,12 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static values = { url: String }
 
+  connect() {
+    if (history.length <= 1) {
+      this.element.classList.remove('is-link')
+    }
+  }
+
   back() {
     if (history.length > 1) {
       history.back()
