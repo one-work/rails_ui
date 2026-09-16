32c1a87879b4b9305e25f3bc54bbc0e97d7eb2b6
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-16 17:22:45 +0800

    his tobygo sync

--- app/javascripts/turbo/actions.js
+++ app/javascripts/turbo/actions.js
@@ -5,10 +5,13 @@ Turbo.StreamActions.visit = function() {
   if (['replace'].includes(action)) {
     Turbo.visit(url || this.baseURI, { action: action })
   } else if (['back'].includes(action)) {
+    window.addEventListener('popstate', (event) => {
+      alert(JSON.stringify(event))
+      if (url) {
+        Turbo.visit(url, { action: 'replace' })
+      }
+    })
     history.back()
-    if (url) {
-      Turbo.visit(url, { action: 'replace' })
-    }
   } else {
     Turbo.visit(url || this.baseURI)
   }
