dcad6a0daedb1b6f08b025a91fdf8f7958b12ba2
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-17 21:59:58 +0800

    flxex

--- app/javascripts/stimulus/com/tr-actions.js
+++ app/javascripts/stimulus/com/tr-actions.js
@@ -16,7 +16,7 @@ export default class extends Controller {
     Array.from(new Set(positions)).forEach(position => {
       const div = document.createElement('div')
       div.dataset.showTarget = 'item'
-      div.classList.add('visibility-hidden', 'flex')
+      div.classList.add('visibility-hidden', 'flex-buttons')
 
       this.dataValue.filter(i => i.position === position).forEach(item => {
         if (item.action) {
